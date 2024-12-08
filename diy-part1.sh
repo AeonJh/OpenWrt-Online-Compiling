839ed18047d2a9495562e89ffda44777f6aaefcf
AeonJh <65694519+AeonJh@users.noreply.github.com>
2024-12-08 17:01:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,4 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
 echo 'src-git 5g_modem_support https://github.com/Siriling/5G-Modem-Support' >>feeds.conf.default
-./scripts/feeds update 5g_modem_support
-./scripts/feeds install luci-app-modem
-sed -i '/src-git 5g_modem_support https:\/\/github.com\/Siriling\/5G-Modem-Support/d;/^$/d' feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
