88e4210ecc61d5f049dc17b7da942c24e3d13c55
AeonJh Fu <aeonjh@nanoleaf.me>
2024-12-25 15:57:42 +0800

    chg: update config.

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -2,7 +2,8 @@
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git 5g_modem_support https://github.com/Siriling/5G-Modem-Support' >>feeds.conf.default
+# echo 'src-git 5g_modem_support https://github.com/Siriling/5G-Modem-Support' >>feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 
 # Modify default IP
-# sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
